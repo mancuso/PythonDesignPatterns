0dd145e5f4a9503ac263e89c6db33240f9403fc9
Peter Mancuso <intercepted@gmail.com>
2021-02-28 23:32:08 -0500

    first test and model

--- architecture/test-batches.py
+++ architecture/test-batches.py
@@ -1,8 +1,11 @@
-import model
+from model import Batch, OrderLine
+from datetime import date
+
 
 def test_allocating_to_a_batch_reduces_the_available_quantity():
     batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=date.today())
     line = OrderLine('order-ref', "SMALL-TABLE", 2)
     batch.allocate(line)
-
     assert batch.available_quantity == 18
+
+test_allocating_to_a_batch_reduces_the_available_quantity()
