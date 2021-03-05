718c094496f465ccb409663a7ecf0c7a864f1883
Peter Mancuso <intercepted@gmail.com>
2021-03-04 22:42:48 -0500

    debug

--- architecture/test-batches.py
+++ architecture/test-batches.py
@@ -6,7 +6,7 @@ def test_allocating_to_a_batch_reduces_the_available_quantity():
     batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=date.today())
     line = OrderLine('order-ref', "SMALL-TABLE", 2)
     batch.allocate(line)
-    assert batch.available_quantity == 17
+    assert batch.available_quantity == 18
 
 def make_batch_and_line(sku, batch_qty, line_qty):
     return (
@@ -22,7 +22,7 @@ def test_can_allocate_if_available_greater_than_required():
 def test_cannot_allocate_if_available_smaller_than_required():
     small_batch, large_line = make_batch_and_line("ELEGANT-LAMP", 2,20)
 
-    assert small_batch.can_allocate(large_line) is Falsev
+    assert small_batch.can_allocate(large_line) is False
     
 def test_can_allocate_if_available_equal_to_required():
     batch, line = make_batch_and_line("ELEGANT-LAMP", 2, 2)
@@ -32,3 +32,9 @@ def test_cannot_allocate_if_skus_do_not_match():
     batch = Batch("batch-001", "UNCOMFORTABLE-CHAIR", 100, eta=None)
     different_sku_line = OrderLine("order-123", "EXPENSIVE-TOASTER",10)
     assert batch.can_allocate(different_sku_line) is False
+
+test_allocating_to_a_batch_reduces_the_available_quantity()
+test_can_allocate_if_available_equal_to_required()
+test_can_allocate_if_available_greater_than_required()
+test_cannot_allocate_if_available_smaller_than_required()
+test_cannot_allocate_if_skus_do_not_match()
